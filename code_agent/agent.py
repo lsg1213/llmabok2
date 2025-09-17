b1e60122e6f9fb812d9660afd2e8033f76c51a8d
chanwon.lee <chanwon.lee@gmail.com>
2025-09-17 15:04:09 +0900

    [add] my codes

--- code_agent/agent.py
+++ code_agent/agent.py
@@ -1,6 +1,10 @@
 from google.adk.agents import Agent
+from google.adk.code_executors import BuiltInCodeExecutor
 
 root_agent = Agent(
     name="code_agent",
     model="gemini-2.0-flash",
+    code_executor=BuiltInCodeExecutor(),
+    instruction="사용자의 요청을 처리하는 코드를 작성하고, 실행 결과를 알려줘.",
+    include_contents='none'
 )
