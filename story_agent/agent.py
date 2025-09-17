068e2e3c5511ffff92db4e8e6966c6995e8fc856
chanwon.lee <chanwon.lee@gmail.com>
2025-09-17 16:20:17 +0900

    [add] sequential agent and loop agent

--- story_agent/agent.py
+++ story_agent/agent.py
@@ -1,5 +1,15 @@
 from google.adk.agents import SequentialAgent, LoopAgent
 from .sub_agents import initial_writer_agent, critic_agent, refiner_agent
 
-root_agent = critic_agent
 
+refiner_critic_loop_agent = LoopAgent(
+    name="loop",
+    sub_agents=[critic_agent, refiner_agent],
+    max_iterations=5,
+)
+
+root_agent = SequentialAgent(
+    name="sequential",
+    sub_agents=[initial_writer_agent, refiner_critic_loop_agent])
+
+# root_agent = initial_writer_agent
--- story_agent/sub_agents.py
+++ story_agent/sub_agents.py
@@ -10,22 +10,22 @@ initial_writer_agent = Agent(
     name="InitialWriterAgent",
     model=GEMINI_MODEL,
     output_key="story",
-    instruction="당신은 창의적인 삼행시를 만드는 작가입니다. 사용자 입력을 기반으로 아주 웃긴 삼행시를 작성해 주세요.",
-    # instruction="당신은 창의적인 이야기를 만드는 작가입니다. 사용자 입력을 기반으로 흥미로운 이야기를 작성해 주세요.",
+    # instruction="당신은 창의적인 삼행시를 만드는 작가입니다. 사용자 입력을 기반으로 아주 웃긴 삼행시를 작성해 주세요.",
+    instruction="당신은 창의적인 이야기를 만드는 작가입니다. 사용자 입력을 기반으로 흥미로운 이야기를 작성해 주세요.",
 )
 
 critic_agent = Agent(
     name="CriticAgent",
     model=GEMINI_MODEL,
     output_key="criticism",
-    instruction="""당신은 삼행시의 비평가입니다. 비판적으로 삼행시를 읽고, 개선할 점을 하나하나 개조식으로 지적해 주시고 markdown 형식으로 반환해주세요. 이 때에 삼행시 형식이 지켜졌는지도 꼭 확인해주세요.
-                   한국어가 아닌 다른 언어로 작성되었다면 해당 부분도 지적해주세요.
-                   만약 삼행시가 충분히 좋다면, **false**를 반환하세요.
-                   이야기: {story}""",
-    # instruction="""당신은 이야기의 비평가입니다. 비판적으로 이야기를 읽고, 개선할 점을 하나하나 개조식으로 지적해 주시고 markdown 형식으로 반환해주세요.
-    #                만약 이야기가 충분히 좋다면, **false**를 반환하세요.
+    # instruction="""당신은 삼행시의 비평가입니다. 비판적으로 삼행시를 읽고, 개선할 점을 하나하나 개조식으로 지적해 주시고 markdown 형식으로 반환해주세요. 이 때에 삼행시 형식이 지켜졌는지도 꼭 확인해주세요.
+    #                한국어가 아닌 다른 언어로 작성되었다면 해당 부분도 지적해주세요.
+    #                만약 삼행시가 충분히 좋다면, **false**를 반환하세요.
     #                이야기: {story}""",
-    # tools=[is_no_criticism],
+    instruction="""당신은 이야기의 비평가입니다. 비판적으로 이야기를 읽고, 개선할 점을 하나하나 개조식으로 지적해 주시고 markdown 형식으로 반환해주세요.
+                   만약 이야기가 충분히 좋다면, **false**를 반환하세요.
+                   이야기: {story}""",
+    tools=[is_no_criticism],
 )
 
 refiner_agent = Agent(
@@ -33,10 +33,10 @@ refiner_agent = Agent(
     model=GEMINI_MODEL,
     output_key="story",
     tools=[exit_loop],
-    instruction="""당신은 삼행시를 다듬는 전문가입니다. **criticism 변수가 false가 아니라면, 비평가의 의견을 반영하여 삼행시를 개선해 주세요**. 
-                   비평가의 의견: {criticism}
-                   만약 {criticism}이 false라면, 'exit_loop' 도구를 사용하여 루프를 종료하세요.""",
-    # instruction="""당신은 이야기를 다듬는 전문가입니다. **criticism 변수가 false가 아니라면, 비평가의 의견을 반영하여 이야기를 개선해 주세요**. 
+    # instruction="""당신은 삼행시를 다듬는 전문가입니다. **criticism 변수가 false가 아니라면, 비평가의 의견을 반영하여 삼행시를 개선해 주세요**. 
     #                비평가의 의견: {criticism}
     #                만약 {criticism}이 false라면, 'exit_loop' 도구를 사용하여 루프를 종료하세요.""",
+    instruction="""당신은 이야기를 다듬는 전문가입니다. **criticism 변수가 false가 아니라면, 비평가의 의견을 반영하여 이야기를 개선해 주세요**. 
+                   비평가의 의견: {criticism}
+                   만약 {criticism}이 false라면, 'exit_loop' 도구를 사용하여 루프를 종료하세요.""",
 )
