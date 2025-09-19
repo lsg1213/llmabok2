6db3bfb65d400d2593af0bcaa0230c1181c15a8f
chanwon.lee <chanwon.lee@gmail.com>
2025-09-19 16:00:47 +0900

    [add] my codes

--- book_agent/agent copy.py
+++ book_agent/agent copy.py
@@ -1,15 +1,13 @@
-from typing import List
 from google.adk.agents import Agent
 
 from sentence_transformers import SentenceTransformer
 model = SentenceTransformer("Qwen/Qwen3-Embedding-0.6B")
 
 from qdrant_client import QdrantClient
-from qdrant_client.models import Filter, FieldCondition, MatchValue, Range
 client = QdrantClient()
 
 from qdrant_client.models import Filter, FieldCondition, MatchValue, Range, MatchText
-def _parse_filter(filters_: dict):
+def _parse_filter(filters: dict):
     """
     filter dict를 Qdrant Filter must 조건 리스트로 변환.
     지원 operator: eq, ne, gt, lt, gte, lte
@@ -24,7 +22,7 @@ def _parse_filter(filters_: dict):
     should_conditions = []
     must_not_conditions = []
 
-    for key, cond in filters_.items():
+    for key, cond in filters.items():
         # 단순 eq: {"author": "한강"}
         if not isinstance(cond, dict):
             must_conditions.append(
@@ -96,25 +94,27 @@ def _parse_filter(filters_: dict):
     )
 
 
-def get_best_sellers(query: str, filter: dict, limit: int = 3) -> List[dict]:
+    
+def get_best_sellers(query: str, filter: dict, top_k: int = 3):
     """
-        주어진 쿼리에 대해 벡터 유사도 검색을 통해 책을 찾아서 반환합니다.
-       
-        Args:
-            query (str): 검색할 쿼리 문자열
-            filter (dict): 필터 조건 딕셔너리. 사용 가능한 key는 author, title, category, price, date가 있으며, 필터 조건은 key : value 또는 key : {operator : value}로 주어집니다. operator에는 "eq", "ne", "gt", "lt", "gte", "lte"가 있습니다. 예: [{"author": "한강"}, {"year": {"gte": 2020}}]
-            limit (int): 반환할 상위 k개의 책 수 (기본값: 3)
+    주어진 쿼리에 대해 벡터 유사도 검색을 통해 베스트셀러를 찾아서 반환합니다.
 
-        Returns:
-            List[dict]: 검색된 책의 리스트, 각 책은 딕셔너리 형태로 반환됩니다.
+    Args:
+        query (str): 검색할 쿼리 문자열
+        filter (dict): 필터 조건 딕셔너리. 사용 가능한 key는 author, title, category, price, date가 있으며, 필터 조건은 key : value 또는 key : {operator : value}로 주어집니다. operator에는 "eq", "ne", "gt", "lt", "gte", "lte"가 있습니다. 예: [{"author": "한강"}, {"year": {"gte": 2020}}]
+        top_k (int): 반환할 상위 k개의 책 수 (기본값: 3)
+
+    Returns:
+        List[dict]: 베스트셀러 리스트, 각 책은 딕셔너리 형태로 반환됩니다.
     """
-    books = client.query_points(
+    near_points = client.query_points(
         collection_name="bestsellers",
         query=model.encode(query).tolist(),
         query_filter=_parse_filter(filter),
-        limit=limit
+        limit=top_k
     )
-    return [book.payload for book in books.points]
+
+    return [point.payload for point in near_points.points]
 
 root_agent = Agent(
     name="book_agent",
--- book_agent/qdrant2.py
+++ book_agent/qdrant2.py
@@ -6,11 +6,9 @@ with open(r"C:\Users\usejen_id\Desktop\llmabok2\book_agent\best-seller-books.jso
 from sentence_transformers import SentenceTransformer
 model = SentenceTransformer("Qwen/Qwen3-Embedding-0.6B")
 
-from qdrant_client.models import PointStruct, VectorParams, Distance
+from qdrant_client.models import PointStruct, VectorParams, Distance, Filter
 from qdrant_client import QdrantClient
-inputs = list(map(lambda book: book['description'], books))
-embeds = model.encode(inputs)
-points = [PointStruct(id=idx+1, vector=embeds[idx].tolist(),
+points = [PointStruct(id=idx+1, vector=model.encode(book["description"]).tolist(),
                       payload=book) for idx, book in enumerate(books)]
 
 client = QdrantClient(url="http://localhost:6333")
@@ -19,8 +17,11 @@ client.recreate_collection(
     vectors_config=VectorParams(size=model.get_sentence_embedding_dimension(), distance=Distance.COSINE),
 )
 client.upsert(collection_name="bestsellers", points=points)
-# result = client.query_points(
-#     collection_name="bestsellers",
-#     query=model.encode("역사와 인간의 관계").tolist(),
-#     limit=3,
-# )
+result = client.query_points(
+    collection_name="bestsellers",
+    query=model.encode("한강 작가의 소설이 아닌 작품 중에서 재미있는 거 알려줘").tolist(),
+    query_filter=Filter(must_not=[{"key": "author", "match": {"value": "한강"}}]),
+    limit=3,
+)
+breakpoint()
+print(result.payloads)
